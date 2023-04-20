f8123f2752eae0e59d1397b9226f827ef15b119f
Trex Choong <guanstrife@gmail.com>
2023-04-20 15:30:18 +0800

    Initial google map implementation to gallery fragment

--- app/src/main/java/com/example/findmyrahmah/MainActivity.kt
+++ app/src/main/java/com/example/findmyrahmah/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.findmyrahmah
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,13 +12,31 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.findmyrahmah.databinding.ActivityMainBinding
+import androidx.lifecycle.lifecycleScope
+import com.example.findmyrahmah.place.Place
+import com.example.findmyrahmah.place.PlaceRenderer
+import com.example.findmyrahmah.place.PlacesReader
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.google.maps.android.clustering.ClusterManager
+import com.google.maps.android.ktx.addCircle
+import com.google.maps.android.ktx.addMarker
+import com.google.maps.android.ktx.awaitMap
+import com.google.maps.android.ktx.awaitMapLoad
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,6 +61,23 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+//        val mapFragment =
+//            supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+//        lifecycleScope.launchWhenCreated {
+//            // Get map
+//            val googleMap = mapFragment.awaitMap()
+//
+//            addClusteredMarkers(googleMap)
+//
+//            // Wait for map to finish loading
+//            googleMap.awaitMapLoad()
+//
+//            // Ensure all places are visible in the map
+//            val bounds = LatLngBounds.builder()
+//            places.forEach { bounds.include(it.latLng) }
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -53,5 +89,89 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun addClusteredMarkers(googleMap: GoogleMap) {
+        // Create the ClusterManager class and set the custom renderer
+        val clusterManager = ClusterManager<Place>(this, googleMap)
+        clusterManager.renderer =
+            PlaceRenderer(
+                this,
+                googleMap,
+                clusterManager
+            )
+
+        // Set custom info window adapter
+        clusterManager.markerCollection.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+
+        // Add the places to the ClusterManager
+        clusterManager.addItems(places)
+        clusterManager.cluster()
+
+        // Show polygon
+        clusterManager.setOnClusterItemClickListener { item ->
+            addCircle(googleMap, item)
+            return@setOnClusterItemClickListener false
+        }
+
+        // When the camera starts moving, change the alpha value of the marker to translucent
+        googleMap.setOnCameraMoveStartedListener {
+            clusterManager.markerCollection.markers.forEach { it.alpha = 0.3f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 0.3f }
+        }
+
+        googleMap.setOnCameraIdleListener {
+            // When the camera stops moving, change the alpha value back to opaque
+            clusterManager.markerCollection.markers.forEach { it.alpha = 1.0f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 1.0f }
+
+            // Call clusterManager.onCameraIdle() when the camera stops moving so that re-clustering
+            // can be performed when the camera stops moving
+            clusterManager.onCameraIdle()
+        }
+    }
+
+    private var circle: Circle? = null
+
+    // [START maps_android_add_map_codelab_ktx_add_circle]
+    /**
+     * Adds a [Circle] around the provided [item]
+     */
+    private fun addCircle(googleMap: GoogleMap, item: Place) {
+        circle?.remove()
+        circle = googleMap.addCircle {
+            center(item.latLng)
+            radius(1000.0)
+            fillColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimaryTranslucent))
+            strokeColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimary))
+        }
+    }
+    // [END maps_android_add_map_codelab_ktx_add_circle]
+
+    private val bicycleIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.colorPrimary)
+        BitmapHelper.vectorToBitmap(this, R.drawable.ic_directions_bike_black_24dp, color)
+    }
+
+    // [START maps_android_add_map_codelab_ktx_add_markers]
+    /**
+     * Adds markers to the map. These markers won't be clustered.
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker {
+                title(place.name)
+                position(place.latLng)
+                icon(bicycleIcon)
+            }
+            // Set place as the tag on the marker object so it can be referenced within
+            // MarkerInfoWindowAdapter
+            marker?.tag = place
+        }
+    }
+    // [END maps_android_add_map_codelab_ktx_add_markers]
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
     }
 }
--- app/src/main/java/com/example/findmyrahmah/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/findmyrahmah/ui/gallery/GalleryFragment.kt
@@ -5,9 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.example.findmyrahmah.BitmapHelper
+import com.example.findmyrahmah.MarkerInfoWindowAdapter
+import com.example.findmyrahmah.R
 import com.example.findmyrahmah.databinding.FragmentGalleryBinding
+import com.example.findmyrahmah.place.Place
+import com.example.findmyrahmah.place.PlaceRenderer
+import com.example.findmyrahmah.place.PlacesReader
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.Circle
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.clustering.ClusterManager
+import com.google.maps.android.ktx.addCircle
+import com.google.maps.android.ktx.addMarker
+import com.google.maps.android.ktx.awaitMap
+import com.google.maps.android.ktx.awaitMapLoad
 
 class GalleryFragment : Fragment() {
 
@@ -16,6 +35,10 @@ class GalleryFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val places: List<Place> by lazy {
+        PlacesReader(requireContext()).read()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,13 +50,105 @@ class GalleryFragment : Fragment() {
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        lifecycleScope.launchWhenCreated {
+            // Get map
+            val googleMap = mapFragment.awaitMap()
+
+            addClusteredMarkers(googleMap)
+
+            // Wait for map to finish loading
+            googleMap.awaitMapLoad()
+
+            // Ensure all places are visible in the map
+            val bounds = LatLngBounds.builder()
+            places.forEach { bounds.include(it.latLng) }
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
         }
         return root
     }
+
+    private fun addClusteredMarkers(googleMap: GoogleMap) {
+        // Create the ClusterManager class and set the custom renderer
+        val clusterManager = ClusterManager<Place>(requireContext(), googleMap)
+        clusterManager.renderer =
+            PlaceRenderer(
+                requireContext(),
+                googleMap,
+                clusterManager
+            )
+
+        // Set custom info window adapter
+        clusterManager.markerCollection.setInfoWindowAdapter(MarkerInfoWindowAdapter(requireContext()))
+
+        // Add the places to the ClusterManager
+        clusterManager.addItems(places)
+        clusterManager.cluster()
+
+        // Show polygon
+        clusterManager.setOnClusterItemClickListener { item ->
+            addCircle(googleMap, item)
+            return@setOnClusterItemClickListener false
+        }
+
+        // When the camera starts moving, change the alpha value of the marker to translucent
+        googleMap.setOnCameraMoveStartedListener {
+            clusterManager.markerCollection.markers.forEach { it.alpha = 0.3f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 0.3f }
+        }
+
+        googleMap.setOnCameraIdleListener {
+            // When the camera stops moving, change the alpha value back to opaque
+            clusterManager.markerCollection.markers.forEach { it.alpha = 1.0f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 1.0f }
+
+            // Call clusterManager.onCameraIdle() when the camera stops moving so that re-clustering
+            // can be performed when the camera stops moving
+            clusterManager.onCameraIdle()
+        }
+    }
+
+    private var circle: Circle? = null
+
+    // [START maps_android_add_map_codelab_ktx_add_circle]
+    /**
+     * Adds a [Circle] around the provided [item]
+     */
+    private fun addCircle(googleMap: GoogleMap, item: Place) {
+        circle?.remove()
+        circle = googleMap.addCircle {
+            center(item.latLng)
+            radius(1000.0)
+            fillColor(ContextCompat.getColor(requireContext(), R.color.colorPrimaryTranslucent))
+            strokeColor(ContextCompat.getColor(requireContext(), R.color.colorPrimary))
+        }
+    }
+    // [END maps_android_add_map_codelab_ktx_add_circle]
+
+    private val bicycleIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(requireContext(), R.color.colorPrimary)
+        BitmapHelper.vectorToBitmap(requireContext(), R.drawable.ic_directions_bike_black_24dp, color)
+    }
+
+    // [START maps_android_add_map_codelab_ktx_add_markers]
+    /**
+     * Adds markers to the map. These markers won't be clustered.
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker {
+                title(place.name)
+                position(place.latLng)
+                icon(bicycleIcon)
+            }
+            // Set place as the tag on the marker object so it can be referenced within
+            // MarkerInfoWindowAdapter
+            marker?.tag = place
+        }
+    }
+    // [END maps_android_add_map_codelab_ktx_add_markers]
 
     override fun onDestroyView() {
         super.onDestroyView()
